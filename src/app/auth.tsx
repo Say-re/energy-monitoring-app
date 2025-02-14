4b13f5cc764a6eacc87e1adf8b135609b33d9d9f
Say-re <91860677+Say-re@users.noreply.github.com>
2025-02-13 20:31:36 -0800

    Task: Create simple sign-in component

--- src/app/auth.tsx
+++ src/app/auth.tsx
@@ -2,18 +2,25 @@
 
 import { useAuth } from "react-oidc-context";
 
+// Styles
+import styles from '../styles/auth.module.css';
+
 function App() {
   const auth = useAuth();
 
   const signOutRedirect = () => {
     const clientId = "52hjjvomou60c0fl7u0slbl85b";
-    const logoutUri = "<logout uri>";
+    const logoutUri = "http://localhost:8080";
     const cognitoDomain = "https://us-west-2gs5pcowrs.auth.us-west-2.amazoncognito.com";
     window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
   };
 
   if (auth.isLoading) {
-    return <div>Loading...</div>;
+    return (
+      <div className={styles['login-screen']}>
+      <div>Loading...</div>
+      </div>
+    );
   }
 
   if (auth.error) {
@@ -34,9 +41,15 @@ function App() {
   }
 
   return (
-    <div>
-      <button onClick={() => auth.signinRedirect()}>Sign in</button>
-      <button onClick={() => signOutRedirect()}>Sign out</button>
+    <div className={styles['login-screen']}>
+      <div>
+        <h1>HEM</h1>
+        <p>Home Energy Monitoring</p>
+        <p>Done, right.</p>
+        <p>Let's get started!</p>
+        {!auth.isAuthenticated && <button onClick={() => auth.signinRedirect()}>Sign in</button>}
+        {auth.isAuthenticated && <button onClick={() => signOutRedirect()}>Sign out</button>}
+      </div>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,65 +2,23 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import { AuthProvider } from "react-oidc-context";
-import App from "./app";
+import Auth from "./auth";
 
 const cognitoAuthConfig = {
   authority: "https://cognito-idp.us-west-2.amazonaws.com/us-west-2_Gs5pCoWrS",
   client_id: "52hjjvomou60c0fl7u0slbl85b",
-  redirect_uri: "http://localhost:8080/auth/success",
+  redirect_uri: "http://localhost:8080",
   response_type: "code",
   scope: "email openid phone",
 };
 export default function Home() {
   return (
     <AuthProvider {...cognitoAuthConfig}>
-    <App />
+    <Auth />
     <div className={styles.page}>
       <main className={styles.main}>
      </main>
       <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
       </footer>
     </div>
     </AuthProvider>
