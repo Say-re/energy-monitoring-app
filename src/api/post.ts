bfc409dd832ba972d8a06e765a9f3d14bf779303
Say-re <91860677+Say-re@users.noreply.github.com>
2025-02-18 08:43:26 -0800

    Task: Add API get and post request functions

--- src/api/post.ts
+++ src/api/post.ts
@@ -1,41 +1,72 @@
-const endpoints = {};
+// Constants
+import ENDPOINTS from '../constants/endpoints';
 
-const postData = async <T, R>(
-  data: T,
-  endpoint: string,
-): Promise<R | {error: string}> => {
-  if (!endpoint || !data) {
-    console.error("Endpoint or data undefined in postData()");
-    return null;
-  }
-  const url = endpoints[endpoint];
+interface postDataProps<T> {
+    auth: string,
+    data: T,
+    headers?: {[key: string]: string},
+    endpoint?: string,
+    customUrl?: string,
+    method?: string,
+}
 
-  if (url && data) {
-    try {
-      const res = await fetch(url, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      }); 
-      if (!res.ok) {
-        console.error("HTTP error occurred while making Post request: ", res.status);
-      }
-      const message = await response.json();
-      
-      return {
-        status: res.status,
-        message,
-      };
-
-    } catch (err) {
-      console.error("Error while making Post request: \n", err.message)
-      return {
-        error: err.message
-      };
+const postData = async <T>({
+    auth,
+    data,
+    headers,
+    endpoint,
+    customUrl,
+    method,
+}:postDataProps<T>): Promise<{ok: boolean, message: string, status: number}> => {
+    if ((!endpoint && !customUrl && !ENDPOINTS?.[endpoint || '']) || !data) {
+        console.error("Endpoint or data undefined in postData()");
+        return {
+            ok: false,
+            message: "Endpoint or data undefined",
+            status: 500,
+        }
     }
-  }
+    const url = customUrl ? customUrl : `${ENDPOINTS.baseUrl}${ENDPOINTS[endpoint || '']}`;
+
+    try {
+        console.log("Data", data);
+        console.log("URL", url);
+        const res = await fetch(url, {
+            method: method || "POST",
+            headers: headers || {
+                "Authorization": auth,
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data)
+        }); 
+        if (!res.ok) {
+            return {
+                message: "HTTP error occurred while making Post request",
+                    status: res.status,
+                ok: false,
+            }
+        }
+        console.log("RESPONSE", res);
+        let message;
+        try {
+            message = await res.json();
+        } catch (err) {
+            message = 'Message unavailable from response';
+        }
+        return {
+            status: res.status,
+            message,
+            ok: true,
+        };
+
+    } catch (err: any) {
+        console.error("Error while making Post request: \n", err.message)
+            return {
+                status: 500,
+                message: err.message,
+                ok: false,
+            };
+    }
 }
 
 export default postData;
