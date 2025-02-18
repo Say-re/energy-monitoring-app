6a321dd0d32cdd3e943ca148562c2fceefbf1d43
Say-re <91860677+Say-re@users.noreply.github.com>
2025-02-18 08:44:54 -0800

    Task: Add in general Authorization component and general styles files

--- src/app/auth.tsx
+++ src/app/auth.tsx
@@ -3,7 +3,7 @@
 import { useAuth } from "react-oidc-context";
 
 // Styles
-import styles from '../styles/auth.module.css';
+import styles from '../styles/auth.module.scss';
 
 function App() {
   const auth = useAuth();
@@ -30,11 +30,6 @@ function App() {
   if (auth.isAuthenticated) {
     return (
       <div>
-        <pre> Hello: {auth.user?.profile.email} </pre>
-        <pre> ID Token: {auth.user?.id_token} </pre>
-        <pre> Access Token: {auth.user?.access_token} </pre>
-        <pre> Refresh Token: {auth.user?.refresh_token} </pre>
-
         <button onClick={() => auth.removeUser()}>Sign out</button>
       </div>
     );
