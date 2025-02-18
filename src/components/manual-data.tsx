37b9c3bd5ed86ac4767275719be3281814099ddf
Say-re <91860677+Say-re@users.noreply.github.com>
2025-02-18 08:48:38 -0800

    Task: Create manual data entry component for users to enter energy consumption at the daily level

--- src/components/manual-data.tsx
+++ src/components/manual-data.tsx
@@ -1,71 +1,90 @@
 import { useState } from 'react';
 // API
-import postData from '../api/post.ts';
+import postData from '../api/post';
 
 // User Auth
 import { useAuth } from "react-oidc-context";
 
 const ManualData = () => {
-  const [date, setDate] = useState<string>(null);
-  const [energy, setEnergy] = useState<number>(null);
-  const [error, setError] = useState<{[key: string]: string}>({});
+    const [date, setDate] = useState<string>('');
+    const [energy, setEnergy] = useState<number>(0);
+    const [error, setError] = useState<{[key: string]: boolean}>({});
 
-  const auth = useAuth();
-  if (date) {
-    const isValid = /^\d{4}-([0][1-9]|1[0-2])-([0][1-9]|[1-2]\d|3[01])$/g.test(date);
-    setError({
-      ...error,
-      date: isValid,
-    });
-  }
-  if (energy) {
-    const isValid = Number(energy) && energy > 0;
-    setError({
-      ...error,
-      energy: isValid,
-    });
-  }
+    const auth = useAuth();
 
-  const handleDateInput = (e) => {
-     if (e.target.value) {
-        setDate(e.target.value);
-     }
-  }
+    const handleDateInput = (e) => {
+        if (e.target.value !== date) {
+            console.log("Val", e.target.value);
+            console.log("Date", date);
+            console.log("Error", error);
 
-  const handleEnergyInput = (e) => {
-    if (e.target.value) {
-      setEnergy(e.target.value);
-    }
-  }
-
-  return (
-    <form>
-      <div>
-        <label for='date'> Please enter the date of energy usage (YYYY-MM-DD) </label>
-        <input id='date' onChange={handleDateInput}/>
-      </div>
-      <div>
-        <label for='energy'> Please enter the total energy usage in (kWh) </label>
-        <input id='energy' onChange={handleEnergyInput}/>
-      </div>
-      <button type='submit' onClick={(e) => {
-        e.preventDefault();
-        const data = {
-          date,
-          energy,
-          accessToken: auth.user.access_token,
-          user_id: auth.user.profile['cognito:username'],
-        };
-        if (data && data.accessToken && data.user_id) {
-          postData({
-            endpoint: '', // Insert Endpoint
-            data,
-          });
+            setDate(e.target.value);
         }
-        console.log('EVENT', e)
-      }}>Submit</button>
-    </form>
-  )
+    }
+
+    const validateDate = () => {
+        const isValid = /^\d{4}-([0][1-9]|1[0-2])-([0][1-9]|[1-2]\d|3[01])$/gi.test(date);
+        setError({
+            ...error,
+            date: !isValid,
+        });
+        return isValid;
+    };
+
+    const handleEnergyInput = (e) => {
+        console.log("Val", e.target.value);
+        console.log("Energy", energy, date);
+        console.log("Error", error);
+        if (e.target.value !== energy) {
+            setEnergy(e.target.value);
+        }
+    }
+
+    const validateEnergy = () => {
+        const isValid = Boolean(Number(energy) && energy > 0);
+        setError({
+            ...error,
+            energy: !isValid,
+        });
+        return isValid;
+
+    }
+
+    return (
+        <form>
+        <div>
+        <label htmlFor='date'> Please enter the date of energy usage (YYYY-MM-DD) </label>
+        <input id='date' onChange={handleDateInput} onBlur={validateDate}/>
+        </div>
+        <div>
+        <label htmlFor='energy'> Please enter the total energy usage in (kWh) </label>
+        <input id='energy' onChange={handleEnergyInput} onBlur={validateEnergy}/>
+        <p>{error.energy}</p>
+        </div>
+        <button type='submit' onClick={async (e) => {
+            e.preventDefault();
+            const isValid = validateEnergy() && validateDate();
+            console.log('errors', error, isValid);
+            if (isValid) {
+                const data = {
+                    date,
+                    energy,
+                    userId: auth.user?.profile?.sub || '',
+                };
+                console.log("Data", data, !error.date);
+                if (data && data.userId && !error.date) {
+                    const res = await postData({
+                        auth: auth.user?.id_token || '',
+                        data,
+                        endpoint: 'input',
+                    });
+                    console.log("REsponse", res);
+                }
+            }
+            console.log('EVENT', e)
+        }}>Submit</button>
+        </form>
+    )
 };
 
 export default ManualData;
